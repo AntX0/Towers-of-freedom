9030f990631032aa9d68ef78f43afb04e30bf566
Anton Pan <antoxaantoxa23@gmail.com>
2022-12-20 21:23:21 +0600

    Target Closest Enemy

--- Assets/Tower/TargetLocator.cs
+++ Assets/Tower/TargetLocator.cs
@@ -1,26 +1,57 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Animations;
+
 
 public class TargetLocator : MonoBehaviour
 {
     [SerializeField] Transform weapon;
     [SerializeField] Transform target;
-
-    void Start()
-    {
-        target = FindObjectOfType<EnemyMover>().transform;
-    }
+    [SerializeField] float range;
+    [SerializeField] ParticleSystem projectileParticles;
 
     void Update()
     {
+        FindClosestTarget();
         AimWeapon();
+    }
+
+    private void FindClosestTarget()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        Transform closestTarget = null;
+        float maxDistance = Mathf.Infinity;
+
+        foreach(Enemy enemy in enemies)
+        {
+            float targetDistance = Vector3.Distance(transform.position, enemy.transform.position);
+            if (targetDistance < maxDistance) 
+            {
+                closestTarget = enemy.transform;
+                maxDistance = targetDistance;
+            }
+        }
+        target = closestTarget;
     }
 
     private void AimWeapon()
     {
+        float targetDistance = Vector3.Distance(transform.position, target.transform.position);
+
         transform.LookAt(target);
+
+        if(targetDistance < range)
+        {
+            Attack(true);
+        }
+        else
+        {
+            Attack(false);
+        }
+    }
+
+    private void Attack(bool isActive)
+    {
+        var emissionModule = projectileParticles.emission;
+        emissionModule.enabled= isActive;
     }
 }
